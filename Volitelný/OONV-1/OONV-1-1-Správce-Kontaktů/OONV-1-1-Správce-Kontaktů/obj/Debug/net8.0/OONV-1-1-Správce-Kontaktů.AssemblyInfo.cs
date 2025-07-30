81e14c1038a27afefb21cf3009e35cd7b670c113
PalacinkaVelika <128801641+TheJosephBear@users.noreply.github.com>
2025-07-30 15:48:26 +0200

    Dokumentace přidána
    Pomocí Sandcastle
    -> Musí být vše zaškrtlé v API
    -> Musí se generovat XML
    -> V sandcastle se musí přidat dll, xml, exe

--- Volitelný/OONV-1/OONV-1-1-Správce-Kontaktů/OONV-1-1-Správce-Kontaktů/obj/Debug/net8.0/OONV-1-1-Správce-Kontaktů.AssemblyInfo.cs
+++ Volitelný/OONV-1/OONV-1-1-Správce-Kontaktů/OONV-1-1-Správce-Kontaktů/obj/Debug/net8.0/OONV-1-1-Správce-Kontaktů.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OONV-1-1-Správce-Kontaktů")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16427d276f97671689a3c2aba27a5d1a9c156fa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e574b97921be707a6ad17fe913cf1d629361b91e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OONV-1-1-Správce-Kontaktů")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OONV-1-1-Správce-Kontaktů")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
